6ace0c1f203ce7591a2bf8b60710b9d37059ff67
Shourya-Sinha <shouryasinha.c@gmail.com>
2024-11-09 20:35:33 +0700

    update app.js

--- Server/app.js
+++ Server/app.js
@@ -35,7 +35,7 @@ app.use(
   })
 );
 
-app.use(process.env.origin, limiter);
+app.use(process.env.ORIGIN, limiter);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
